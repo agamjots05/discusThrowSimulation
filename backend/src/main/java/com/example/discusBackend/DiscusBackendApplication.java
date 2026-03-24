ea2d4de52e606393a2b1374d0116e9b2bbd7407a
agamjots <email.agamjotsingh@gmail.com>
2026-03-23 23:06:49 -0700

    init spring boot backend

---
+++ backend/src/main/java/com/example/discusBackend/DiscusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.discusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscusBackendApplication.class, args);
+	}
+
+}
